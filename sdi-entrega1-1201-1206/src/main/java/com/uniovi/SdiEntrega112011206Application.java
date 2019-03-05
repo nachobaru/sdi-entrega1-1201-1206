7bb4f64f7d3b9f893236045a8b349e0dd7a8a3f5
nachobaru <ignaciobaruque@gmail.com>
2019-03-05 13:37:08 +0100

    primer commit.

---
+++ sdi-entrega1-1201-1206/src/main/java/com/uniovi/SdiEntrega112011206Application.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiEntrega112011206Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdiEntrega112011206Application.class, args);
+	}
+
+}
