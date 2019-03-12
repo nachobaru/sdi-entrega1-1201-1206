b4b0b58fb656f5cc961d7f6603e2abbfe50c42cc
Alejandro Iglesias Calvo <uo24346@uniovi.es>
2019-03-12 15:45:03 +0100

    Commit de prueba a mi branch

--- sdi-entrega1-1201-1206/src/main/java/com/uniovi/SdiEntrega112011206Application.java
+++ sdi-entrega1-1201-1206/src/main/java/com/uniovi/SdiEntrega112011206Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SdiEntrega112011206Application {
-//pedo
+//PRUEBAAAAAAAAAAAA
 	public static void main(String[] args) {
 		SpringApplication.run(SdiEntrega112011206Application.class, args);
 	}
