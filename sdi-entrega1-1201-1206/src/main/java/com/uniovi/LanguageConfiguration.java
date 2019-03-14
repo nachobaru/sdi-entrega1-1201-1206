2af77a9a90eb73824189e6c2235a07dd823d5c0a
nachobaru <ignaciobaruque@gmail.com>
2019-03-14 14:01:55 +0100

    12_1

--- sdi-entrega1-1201-1206/src/main/java/com/uniovi/LanguageConfiguration.java
+++ sdi-entrega1-1201-1206/src/main/java/com/uniovi/LanguageConfiguration.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -13,25 +14,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+@Configuration
 public class LanguageConfiguration implements WebMvcConfigurer {
 	@Bean
 	public LocaleResolver localeResolver() {
-		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-		localeResolver.setDefaultLocale(new Locale("es", "ES"));
-		return localeResolver;
+	SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+	localeResolver.setDefaultLocale(new Locale("es", "ES"));
+	return localeResolver;
 	}
-
 	@Bean
 	public LocaleChangeInterceptor localeChangeInterceptor() {
-		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
-		localeChangeInterceptor.setParamName("lang");
-		return localeChangeInterceptor;
+	LocaleChangeInterceptor localeChangeInterceptor =
+	new LocaleChangeInterceptor();
+	localeChangeInterceptor.setParamName("lang");
+	return localeChangeInterceptor;
 	}
-
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(localeChangeInterceptor());
+	registry.addInterceptor(localeChangeInterceptor());
 	}
+
 	
 	@Override
 	 public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
--- sdi-entrega1-1201-1206/src/main/java/com/uniovi/services/ArticleService.java
+++ sdi-entrega1-1201-1206/src/main/java/com/uniovi/services/ArticleService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.uniovi.entities.Article;
-import com.uniovi.entities.User;
 import com.uniovi.repositories.ArticleRepository;
 
 @Service
